74b4ff2a169c7a6392c8dfecee377e0aa08e0d13
dt310732 <dt31072@zpsb.pl>
2026-05-28 15:14:49 +0200

    dev

--- django_learning/core/admin.py
+++ django_learning/core/admin.py
@@ -1,1 +1,27 @@
 from django.contrib import admin
+from .models import Category, Supplier, Part, Location, StockMovement, PartLocation
+
+admin.site.register(Category)
+admin.site.register(Supplier)
+admin.site.register(StockMovement)
+
+class PartLocationInline(admin.TabularInline):
+    model = PartLocation
+    extra = 1
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    list_display = ("item_code", "name", "unit", "is_active")
+    search_fields = ("item_code", "name")
+    inlines = [PartLocationInline]
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "is_active")
+    search_fields = ("code", "name")
+    inlines = [PartLocationInline]
+
+@admin.register(PartLocation)
+class PartLocationAdmin(admin.ModelAdmin):
+    list_display = ("part", "location", "quantity", "is_default")
+    search_fields = ("part__item_code", "part__name", "location__code")
--- django_learning/core/models.py
+++ django_learning/core/models.py
@@ -21,6 +21,15 @@ class Supplier(models.Model):
     def __str__(self):
         return self.name
 
+class Location(models.Model):
+    code = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.code
+
 
 class Part(models.Model):
     PART_UNITS = [
@@ -31,7 +40,7 @@ class Part(models.Model):
     ]
     category = models.ForeignKey(
         Category,
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         related_name="parts",
         null=False
     )
@@ -41,25 +50,49 @@ class Part(models.Model):
         related_name="parts",
         null=True
     )
-    item_code = models.IntegerField(unique=True)
+    locations = models.ManyToManyField(
+        Location,
+        through="PartLocation",
+        related_name="parts",
+        blank=True
+    )
+    item_code = models.CharField(max_length=100 ,unique=True, blank=False)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     unit = models.CharField(max_length=3, choices=PART_UNITS)
-    minimum_stock = models.PositiveIntegerField() # zrobić większe od 0 
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        supplier_name = self.supplier.name if self.supplier else "No Supplier"
-        return f'{self.name} - {self.category.name} - {supplier_name}'
+        return f'{self.name}'
     
+class PartLocation(models.Model):
+    part = models.ForeignKey(
+        Part,
+        on_delete=models.CASCADE,
+        related_name="part_locations"
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name="part_locations"
+    )
 
-class Location(models.Model):
-    code = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    is_active = models.BooleanField(default=True)
+
+    quantity = models.PositiveIntegerField(default=0)
+    is_default = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["part", "location"],
+                name= "unique_part_location"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.part.item_code} - {self.location.code} - {self.quantity}"
 
 class StockMovement(models.Model):
     MOVEMENT_TYPES = [
@@ -68,13 +101,13 @@ class StockMovement(models.Model):
     ("MOVE", "Przesunięcie"),
     ("SCRAP", "Złomowanie"),
     ]
-    part = models.ForeignKey(
-        Part,
-        on_delete=models.CASCADE
-    )
-    location = models.ForeignKey(
-        Location,
-        on_delete=models.CASCADE
+
+    part_location = models.ForeignKey(
+        PartLocation,
+        on_delete=models.PROTECT,
+        related_name="stock_movement",
+        null=True,
+        blank=True
     )
     movement_type = models.CharField(max_length=20, choices=MOVEMENT_TYPES)
     quantity = models.IntegerField(null=False)
